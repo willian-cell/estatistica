40373cc292fc642fe2e647b3dae73e73f0f10e25
EFG <efg@efg.local>
2025-06-06 20:28:49 -0300

    mmoses

--- media_ponderada.py
+++ media_ponderada.py
@@ -37,3 +37,49 @@ print(f">>> Média Ponderada  das Temperatudar: {media_sensores:.2f}")
 
 media_simples = sum(all_data) / len(all_data)
 print(f"Média Aritimética Simples das Temperaturas: {media_simples:.2f} °C")
+
+
+# --- Exemplo 2: Média Ponderada para Experimento Online (Correção de Representatividade Desigual) ---
+
+# Dados de satisfação dos usuários (escala de 1 a 10)
+# Usuários Ativos (15 respostas)
+satisfacao_usuarios_ativos = [8, 9, 7, 8, 9, 10, 7, 8, 9, 8, 9, 7, 10, 8, 9]
+# Usuários Ocasionais (10 respostas)
+satisfacao_usuarios_ocasionais = [5, 6, 7, 4, 5, 6, 7, 4, 5, 6]
+
+amostra_completa = satisfacao_usuarios_ativos + satisfacao_usuarios_ocasionais
+
+proporcao_populcacao_ativa = 0.70  # 70% de todos sao ativos
+proporcao_populcacao_ocasional = 0.30 # 30% de todos ocasionais
+
+proporcao_ativa_amostra = len(satisfacao_usuarios_ativos) / len(amostra_completa)
+proporcao_ocasional_amostra = len(satisfacao_usuarios_ocasionais) / len(amostra_completa)
+
+print("___"*30)
+print(f"Proporção usuários ativos na amostra: {proporcao_ativa_amostra}")
+print(f"Proporção usuários ocasionais: {proporcao_ocasional_amostra} ")
+
+peso_ativos_balanceado = proporcao_populcacao_ativa / proporcao_ativa_amostra
+peso_ocasionais_balanceado = proporcao_populcacao_ocasional / proporcao_ocasional_amostra
+
+array_pesos_ativo = [peso_ativos_balanceado] * len(satisfacao_usuarios_ativos)
+array_pesos_ocasionais = [peso_ocasionais_balanceado] * len(satisfacao_usuarios_ocasionais)
+
+array_todos_pesos = array_pesos_ativo + array_pesos_ocasionais 
+
+# -- Cálculo
+
+soma_valores_ponderados = 0
+soma_pesos = 0
+
+for i in range(len(amostra_completa)):
+    valor = amostra_completa[i]
+    peso = array_todos_pesos[i]
+    soma_valores_ponderados += (valor * peso)
+    soma_pesos += peso
+
+media_satisfacao_ponderada = soma_valores_ponderados / soma_pesos
+print(f"\n Média Ponderada da Satisfação do usuário: {media_satisfacao_ponderada:.2f}")
+
+media_simples = sum(amostra_completa) / len(amostra_completa)
+print(f"\n Média simples da Satisfação do usuário: {media_simples:.2f}")
