03ff6f6e06b5a11f46cd7753c2e34547b1c208ec
EFG <efg@efg.local>
2025-06-16 19:53:20 -0300

    tamos

--- percentis.py
+++ percentis.py
@@ -22,7 +22,6 @@ df_alunos = pd.DataFrame({
 })
 print(f"\n--- DataFrame Alunos e Notas ---")
 print(f"\n{df_alunos.head()}")
-print(f"\n -------------------------------")
 
 # Cálculos de Quartis 
 q1 = df_alunos['Nota'].quantile(0.25, )
@@ -33,4 +32,30 @@ df_quartis = pd.DataFrame({
     'Quartil': ['Primeiro (Q1)', 'Segundo (Q2 = Mediana)', 'Terceiro (Q3)'],
     'Valor': [q1, q2, q3]
 })
+
+print("\n", "="*30, "\n")
 print(df_quartis)
+
+# Calculos de Decis
+decis_percentis = [i / 10.0 for i in range(1, 11)]
+decis_valores = [df_alunos['Nota'].quantile(p) for p in decis_percentis]
+decis_nomes = [f"{i}° Decil" for i in range(1, 11)]
+df_decis = pd.DataFrame({
+    'Decil': decis_nomes, 
+    'Percentil': [f"{int(p*100)}%" for p in decis_percentis],
+    'Valor': decis_valores
+})
+
+print("="*30, "Decis das notas", "="*30)
+print(df_decis)
+print("\n")
+
+amplitude_interquatil = q3 - q1
+df_aiq = pd.DataFrame({
+    'Métrica': ['Amplitude Interquatil (AIQ)'],
+    'Valor': [amplitude_interquatil]
+})
+
+print("--- Amplitude Interquartil ---")
+print(df_aiq)
+print("\n")
